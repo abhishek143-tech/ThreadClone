2eb578b279492ab2dab6ea8fb903e4773c3a5587
Abhishek Dhok <abhishekdhok96@gmail.com>
2023-10-01 14:29:18 +0530

    Created Tread Creation UI

--- ThreadsClone/Core/TabBar/ThreadsTabView.swift
+++ ThreadsClone/Core/TabBar/ThreadsTabView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ThreadsTabView: View {
 
     @State private var selectedTab = 0
+    @State private var showCreateThreadView = false
 
     var body: some View {
         TabView(selection: $selectedTab) {
@@ -28,7 +29,7 @@ struct ThreadsTabView: View {
                 .onAppear { selectedTab = 1 }
                 .tag(1)
 
-            ThreadCreationView()
+            Text("")
                 .tabItem {
                     Image(systemName: "plus")
                 }
@@ -54,6 +55,14 @@ struct ThreadsTabView: View {
                 .tag(4)
 
         }
+        .onChange(of: selectedTab, perform: { newValue in
+            showCreateThreadView = selectedTab == 2
+        })
+        .sheet(isPresented: $showCreateThreadView, onDismiss: {
+            selectedTab = 0
+        }, content: {
+            ThreadCreationView()
+        })
         .tint(.black)
     }
 }
